6ad15aceb8182349fc579ec1ae0bc9b98af22ac1
Wilson Dos Santos <wilson-senx@hotmail.com>
2022-11-17 19:59:30 -0300

    :sparkles: Create UI Web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyles />
     </div>
   );
 }
